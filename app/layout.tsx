642308a3b5968505e0aef23f1743f79e979dc5a4
Moses Nwigberi <mosesnwigberi@gmail.com>
2024-02-02 19:05:29 +0100

    Update import path for Layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/components/layout";
+import Layout from "../components/Layout";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script";
