f9bd05a047017ea99e8474b67ba87008d4d5f191
Moses Nwigberi <mosesnwigberi@gmail.com>
2024-02-04 21:05:10 +0100

    Remove Suspense and Loading component from Contact and Donations pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,15 +1,11 @@
 import dynamic from "next/dynamic";
 const ContactPage = dynamic(() => import("../../components/ContactPage"));
-import { Suspense } from "react";
-import Loading from "../loading";
 
 const Contact = () => {
   return (
-    <Suspense fallback={<Loading />}>
-      <div className="contactpage">
-        <ContactPage />
-      </div>
-    </Suspense>
+    <div className="contactpage">
+      <ContactPage />
+    </div>
   );
 };
 
--- app/donations/page.tsx
+++ app/donations/page.tsx
@@ -1,13 +1,7 @@
 import dynamic from "next/dynamic";
 const DonationsPage = dynamic(() => import("../../components/DonationsPage"));
-import { Suspense } from "react";
-import Loading from "../loading";
 const Donations = () => {
-  return (
-    <Suspense fallback={<Loading />}>
-      <DonationsPage />
-    </Suspense>
-  );
+  return <DonationsPage />;
 };
 
 export default Donations;
