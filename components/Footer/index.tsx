b4f3e0a0750b4728a71fb6569db84513ea53925a
Moses Nwigberi <mosesnwigberi@gmail.com>
2024-02-09 13:47:41 +0100

    Footer component: UI/UX improvements and responsiveness

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,130 +2,108 @@ import styles from "./index.module.css";
 import logo from "../../public/images/greencal_white.png";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Footer = () => {
+  const router = useRouter();
   return (
     <div className={styles.footer}>
       <div className={styles.grid}>
         <div>
-          <div className={styles.logo}>
+          <div onClick={() => router.push("/")} className={styles.logo}>
             <Image
               src={logo}
-              style={{
-                objectFit: "cover",
-              }}
-              alt="Greencal-foundation logo"
+              alt="card-image"
               fill
               quality={100}
               priority={true}
               sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             />
           </div>
-          <p>
-            Giving help To Those Who Need It. Charity works make the world a
-            better place.
-          </p>
-          <strong>Opening Hours :</strong> Monday Sat: 9am to 6pm
-          <div className={styles.icons}>
-            <i className="fa-brands fa-facebook"></i>
-            <i className="fa-brands fa-twitter"></i>
-            <i className="fa-brands fa-pinterest"></i>
-            <i className="fa-brands fa-instagram"></i>
-          </div>
         </div>
         <div>
           <h2>GET SUPPORT</h2>
           <div>
-            <div>
+            <div className={styles.calIcons}>
               <i className="fa-solid fa-location-dot"></i>
             </div>
             <div>Nepa Junction, Mile 50, Abakaliki, Ebonyi, Nigeria</div>
           </div>
 
-          <div>
+          <div className={styles.calIcons}>
             <div>
               <i className="fa-solid fa-phone"></i>
             </div>
             <div>(+234) 807 548 9362, (+880) 155 69569 366</div>
           </div>
-          <div>
+          <div className={styles.calIcons}>
             <div style={{ width: "50px" }}>
               <i className="fa-solid fa-envelope"></i>
             </div>
             <div>info@greencal.com, info.greencal@yahoo.com</div>
           </div>
         </div>
+
         <div>
-          <h2>OUR SERVICES</h2>
+          <h2>COMPANY</h2>
           <div>
-            <i className="fa fa-angle-right"></i>
-            Donations
+            <Link href={"/#about"}>
+              <i className="fa fa-angle-right"></i>
+              About Us
+            </Link>
           </div>
           <div>
-            <i className="fa fa-angle-right"></i>
-            Grants and Scholarships
+            <Link href={"/donations"}>
+              <i className="fa fa-angle-right"></i>
+              Donations
+            </Link>
           </div>
           <div>
-            <i className="fa fa-angle-right"></i>
-            Education.
+            <Link href={"/contact"}>
+              <i className="fa fa-angle-right"></i>
+              Contact Us
+            </Link>
           </div>
           <div>
-            <i className="fa fa-angle-right"></i>
-            Outreach
+            {/* <i className="fa fa-angle-right"></i> */}
+            <div className={styles.icons}>
+              <a
+                target={`_blank`}
+                rel="noopener noreferrer"
+                href="https://web.facebook.com/Moses.Nwigberi/"
+              >
+                <i className="fa-brands fa-facebook"></i>
+              </a>
+              <a
+                target={`_blank`}
+                rel="noopener noreferrer"
+                href="https://twitter.com/pepnops"
+              >
+                <i className="fa-brands fa-twitter"></i>
+              </a>
+
+              <a
+                target={`_blank`}
+                rel="noopener noreferrer"
+                href="https://www.linkedin.com/company/pepnops/"
+              >
+                <i className="fa-brands fa-linkedin"></i>
+              </a>
+              <a
+                target={`_blank`}
+                rel="noopener noreferrer"
+                href="https://instagram.com/pepnops"
+              >
+                <i className="fa-brands fa-instagram"></i>
+              </a>
+            </div>
           </div>
         </div>
       </div>
 
       <div className={styles.copyright}>
-        <div>© 2024 Greencal Foundation.</div>
-        <div>
-          <ul>
-            <li>
-              <Link href={"/"}>
-                Home <i className="fas fa-circle"></i>
-              </Link>
-            </li>
-            <li>
-              <Link href={"/#about"}>
-                About <i className="fas fa-circle"></i>
-              </Link>
-            </li>
-            <li>
-              <Link href={"/#services"}>
-                Services <i className="fas fa-circle"></i>
-              </Link>
-            </li>
-            <li>
-              <Link href={"/#testimonials"}>
-                Testimonials <i className="fas fa-circle"></i>
-              </Link>
-            </li>
-            <li>
-              <Link href={"/donations"}>
-                Donations <i className="fas fa-circle"></i>
-              </Link>
-            </li>
-            <li>
-              <Link href={"/contact"}>
-                Contact <i className="fas fa-circle"></i>
-              </Link>
-            </li>
-          </ul>
-        </div>
+        <div>© 2024 Greencal Foundation </div>
       </div>
-      {/* <div className={styles.credits}>
-        <p>
-          Designed and developed by{" "}
-          <a
-            target={`_blank`}
-            rel="noopener noreferrer"
-            href="https://github.com/TheMoeEntity"
-          >
-            Moses Nwigberi{" "}
-          </a>
-          - Freelance Web Developer
-        </p>
-      </div> */}
     </div>
   );
 };
